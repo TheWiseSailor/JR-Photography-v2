d774781a11c083118f95e13daba94f1a34515584
TheWiseSailor <rudasilljay@gmail.com>
2023-08-12 13:21:29 -0400

    added another sliding dunction to the contacts

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 let lastScrollTop = 0;
 
-window.addEventListener("scroll", () => {
+document.addEventListener("scroll", () => {
   setTimeout(() => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
@@ -21,4 +21,33 @@ window.addEventListener("load", () => {
     const meSection = document.querySelector(".me");
     meSection.classList.add("active");
   }, 3000);
+});
+
+//contact
+
+document.addEventListener("scroll", () => {
+  setTimeout(() => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const aboutMe = document.querySelector(".about-me");
+    const meSection = document.querySelector(".me");
+    const contactsSection = document.querySelector(".contacts");
+
+    if (scrollTop > lastScrollTop) {
+      aboutMe.classList.add("active");
+      meSection.classList.remove("active");
+    } else {
+      meSection.classList.add("active");
+      aboutMe.classList.remove("active");
+    }
+
+    // Check if aboutMe section is in view
+    const aboutMeTop = aboutMe.offsetTop;
+    if (aboutMeTop - scrollTop < window.innerHeight) {
+      contactsSection.classList.add("active");
+    } else {
+      contactsSection.classList.remove("active");
+    }
+
+    lastScrollTop = scrollTop;
+  }, 12000);
 });
